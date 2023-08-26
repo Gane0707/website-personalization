23aca63fbfe11f93c2d47fe531a5f7ddc54290d5
shahul Fareed <shahulfareed.k@zoominfo.com>
2023-08-27 01:00:02 +0530

    visitor based config added

--- config/visitor.js
+++ config/visitor.js
@@ -1,18 +1,45 @@
 const visitor =[
     {
         id:"visitor1",
-        company:"Amazon",
-        employeeRange:5000
-    },
-    {
-        id:"visitor1",
-        company:"Amazon",
-        employeeRange:5000
-    },
-    {
-        id:"visitor1",
-        company:"Amazon",
-        employeeRange:5000     
-    },
+        city: "Chennai",
+        companyFunding: "2000",
+        companyName: "amazon",
+        country: "india",
+        employeeCount: 5116,
+        ip: "183.82.206.100",
+        primaryIndustry: "Software",
+        revenue: 64788000,
+        state: "Tamil Nadu",
+        website: "www.latentview.com",
+        zipCode: "600113"
+        },
+        {
+            id:"visitor2",
+            city: "Chennai",
+            companyFunding: "2000",
+            companyName: "LatentView",
+            country: "India",
+            employeeCount: 316,
+            ip: "183.82.206.100",
+            primaryIndustry: "Construction",
+            revenue: 8000,
+            state: "Tamil Nadu",
+            website: "www.latentview.com",
+            zipCode: "600113"
+            },
+            {
+                id:"visitor3",
+                city: "Chennai",
+                companyFunding: "2000",
+                companyName: "zoominfo",
+                country: "India",
+                employeeCount: 1116,
+                ip: "183.82.206.100",
+                primaryIndustry: "Software",
+                revenue: 64788000,
+                state: "Tamil Nadu",
+                website: "www.amazon.com",
+                zipCode: "600113"
+            },
 ]
 module.exports = visitor
