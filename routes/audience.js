b78922023bdd3bf5add66b92b86c9424cf43c8e7
S Ganesh <30747241+Gane0707@users.noreply.github.com>
2023-08-26 22:02:53 +0530

    Update audience.js

--- routes/audience.js
+++ routes/audience.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const appservices = require('../services/audience')
 
-
+router.get('/health-check',(req,res)=>{res.send("hello")});
 router.get('/list',appservices.getAudience);
 router.post('/save',appservices.saveAudience);
 
