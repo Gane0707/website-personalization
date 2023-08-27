f9f82eb5785de09c1f98b3ca9e34afeb9eb4d417
S Ganesh <30747241+Gane0707@users.noreply.github.com>
2023-08-27 13:40:14 +0530

    Update audience.js

--- routes/audience.js
+++ routes/audience.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const appservices = require('../services/audience')
 
-app.use((req,res,next)=>{res.setHeader('Access-Control-Allow-Origin', '*')next(););
 
 router.get('/health-check',(req,res)=>{res.send("hello")});
 router.get('/lists',appservices.getAudience);
