84d98604801b41dd6fc113af5e927234d2ad83b7
S Ganesh <30747241+Gane0707@users.noreply.github.com>
2023-08-27 13:44:41 +0530

    Update cors.js

--- routes/cors.js
+++ routes/cors.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Headers')
+  res.setHeader('Access-Control-Allow-Headers','*')
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 };
